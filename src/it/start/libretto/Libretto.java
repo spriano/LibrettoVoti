b0c0b3ed481b7b8fa8eb29699687f3c7e8069b9f
aldos <aldos@DESKTOP-376EK4A.homenet.telecomitalia.it>
2020-06-07 11:31:01 +0200

    classe Libretto

--- src/it/start/libretto/Libretto.java
+++ src/it/start/libretto/Libretto.java
@@ -1,5 +1,17 @@
 package it.start.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	private List<Voto> voti;
+	
+	public Libretto() {
+		voti = new ArrayList<Voto>();
+	}
+	
+	public void add(Voto v) {
+		voti.add(v);
+	}
 
 }
